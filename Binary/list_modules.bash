62698e87034af6a4a4d018293179c74e9303a87a
Yi-Chien Chang <ycchang@ycchang-HP-Mini.(none)>
2013-11-25 00:34:45 -0500

    Output both stdout and stderr to stdout; /opt/XBMC/shell/module.rc will output status to stderr!!!

--- Binary/list_modules.bash
+++ Binary/list_modules.bash
@@ -2,7 +2,7 @@
 for D in /opt/*;do
   if [ -d "${D}" -a -f "${D}/shell/module.rc" ];then
     module=`echo ${D}|cut -f3 -d'/'`;
-    usage=`"${D}/shell/module.rc" | cut -f2 -d'{' | cut -f1 -d'}' | sed 's/ //g'`
+    usage=`"${D}/shell/module.rc"  1>&1 2>&1 | cut -f2 -d'{' | cut -f1 -d'}' | sed 's/ //g'`
     #status="";
     #if [[ "${usage}" =~ "status" ]];then
     #  status=`${D}/shell/module.rc status`
